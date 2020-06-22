ae971df320f7fd697a6b02d41fe4ce8d1b6a30f2
“ <809740443@qq.com>
2020-06-23 00:25:16 +0800

    fix: 修改路由

--- src/router.js
+++ src/router.js
@@ -1,21 +1,21 @@
-import React, { Suspense, lazy } from 'react';
-import { Route, Switch } from 'react-router-dom';
-const Login = lazy(() => import('./page/Login/index'));
-const Home = lazy(() => import('./page/Home/index'));
-const Register = lazy(() => import('./page/Register/index'));
-const Password = lazy(() => import('./page/Password/index'));
+import React, { Suspense, lazy } from "react";
+import { Route, Switch } from "react-router-dom";
+const Login = lazy(() => import("./page/Login/index"));
+const Home = lazy(() => import("./page/Home/index"));
+const Register = lazy(() => import("./page/Register/index"));
+const Password = lazy(() => import("./page/Password/index"));
 
 const Router = () => {
   return (
     <Suspense fallback="">
       <Switch>
-        <Route path="/" component={Home} exact/>
-        <Route path="/login" component={Login}/>
-        <Route path="/register" component={Register}/>
-        <Route path="/password" component={Password}/>
+        <Route path="/home" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <Route path="/password" component={Password} />
       </Switch>
-    </Suspense>  
-  )
-}
+    </Suspense>
+  );
+};
 
-export default Router
+export default Router;
